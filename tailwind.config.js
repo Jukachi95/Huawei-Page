c4ea0ed81c27ab84c71ae3b60c050ac5d68d70e3
Joel Okoro <jukachi95@gmail.com>
2020-11-05 15:45:19 +0000

    Solution for scaling header showcase image

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+    height:{
+      xl: '450px'
+    }
+  },
+  variants: {},
+  plugins: [],
+}
